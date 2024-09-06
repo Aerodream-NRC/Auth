26bb13ecd1168968c00fb767ede3545a72c1529e
Aerodream <nrc.aerodream@gmail.com>
2024-09-06 16:12:53 +0300

    2

---
+++ src/main/java/SocialNetwork/Auth/Services/UserService.java
@@ -0,0 +1,13 @@
+package SocialNetwork.Auth.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+//TODO
+//@Service
+//public class UserService implements UserDetailsService {
+//
+//
+//    @Autowired
+//    private UserService userService;
+//}
